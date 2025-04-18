f21e2d562255326b5815a0a116e95f3e237a269d
Amit Kumar <127661512+amit829972ak@users.noreply.github.com>
2025-04-18 21:37:56 +0530

    Update e.py

--- e.py
+++ e.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import nltk
+nltk.download("punkt")
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 import seaborn as sns
